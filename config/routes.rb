8b93cb5ab68b2c49d58df3acb4d97edf8bac3e7a
igacio <ignacio.olea.c@gmail.com>
2018-11-15 21:24:23 -0300

    Finishing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'students#index'
   resources :students
+  
   get 'dashboards/index'
-  root 'dashboards#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
